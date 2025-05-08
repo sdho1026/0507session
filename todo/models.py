835382e24e741a2afb8c55387d1f0699cd8167cf
songh020202@gmail.com <songhe02@naver.com>
2025-05-09 04:08:38 +0900

    추가

---
+++ todo/models.py
@@ -0,0 +1,15 @@
+# todo/models.py
+
+from django.db import models
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+        
+from django.contrib import admin
+admin.site.register(Todo)
